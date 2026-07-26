d0bc1b1f41fce6b1e383606d2265f31dd75051a9
حسن علی <hassanalik7581@gmail.com>
2026-07-27 01:04:42 +0500

    Implement Column-wise sum calculation for a 2D array

---
+++ Array/30_Column_Wise_Sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3];
+    int r = 3;
+    int c = 3;
+    cout << " Enter elements of 2d array " << endl;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int j=0;j<c;j++){
+        int sum=0;
+        for(int i=0;i<r;i++){
+            sum+=arr[i][j];
+        }
+        cout << " Sum of Column " << j+1 << " = " << sum << endl;
+    }
+    return 0;
+}
