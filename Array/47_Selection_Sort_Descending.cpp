db1e6e5a87a460a20dbe98fb060e127694d9ed0a
حسن علی <hassanalik7581@gmail.com>
2026-08-04 22:53:32 +0500

    Impkemented descending order sorting using selection sort

---
+++ Array/47_Selection_Sort_Descending.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,4,5,3,1};
+    int n=5;
+    for(int i=0;i<n-1;i++){
+       int smallestindex=i;
+       for(int j=i+1;j<n;j++){
+        if(arr[j]>arr[smallestindex]){
+            smallestindex=j;
+        }
+       }
+       swap(arr[i],arr[smallestindex]);
+    }
+    cout << " Descending order Sorted Array " << endl;
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
