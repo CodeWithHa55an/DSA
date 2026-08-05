35acae03a2291477aaa7a075fd3c590c23fa34c3
حسن علی <hassanalik7581@gmail.com>
2026-08-05 17:15:48 +0500

    Implemented Insertion sort in descending order

---
+++ Array/50_Insertion_sort_Descending.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {2,4,5,3,1};
+    int n=5;
+    for(int i=1;i<n;i++){
+        int current = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]<current){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1]=current;
+    }
+    cout << " Sorted Array " << endl;
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
