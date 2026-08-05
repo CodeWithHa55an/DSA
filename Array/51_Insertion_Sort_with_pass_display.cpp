b44937a61b1bce99f413093e84373e0d9c5a3930
حسن علی <hassanalik7581@gmail.com>
2026-08-05 17:16:42 +0500

    Implemented Insertion sort with pass display

---
+++ Array/51_Insertion_Sort_with_pass_display.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void printarray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+}
+int main(){
+    int arr[5]={2,4,5,3,1};
+    int n=5;
+    
+    for(int i=1;i<n;i++){
+        int current = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]>current){
+           arr[prev+1] = arr[prev];
+           prev--;
+        }
+        arr[prev+1] = current;
+        cout << " Pass " << i << " : ";
+        printarray(arr,n);
+        cout << endl;
+    }
+
+}
