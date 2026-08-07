1f6b36ccda5b0984b213f18120c64ae6b06af157
حسن علی <hassanalik7581@gmail.com>
2026-08-07 15:59:28 +0500

    Calculate the sum of a dynamically allocated array

---
+++ Array/62_Sum_Of_Dynamic_Array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout << " Enter size " << endl;
+    cin >> size;
+    int *arr = new int[size];
+    int sum=0;
+    cout << " Enter array elements : " << endl;
+    for(int i=0;i<size;i++){
+        cin>>*(arr+i);
+        sum+=*(arr+i);
+    }
+    cout << " Sum = : " << sum << endl;
+    return 0;
+}
