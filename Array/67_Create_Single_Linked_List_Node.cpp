de1b54609fd057beeac5b875a734f627e2547c28
حسن علی <hassanalik7581@gmail.com>
2026-08-10 23:27:12 +0500

    implementeed single linked list node

---
+++ Array/67_Create_Single_Linked_List_Node.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+};
+int main(){
+Node* head = new Node;
+head->data = 10;
+head->next = NULL;
+cout << " Data = " << head->data << endl;
+delete head;
+return 0;
+}
