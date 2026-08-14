581bc815c2ec0001267cbbb2af9c648902889c4d
حسن علی <hassanalik7581@gmail.com>
2026-08-14 07:06:01 +0500

    Implemented Pop-Front

---
+++ Array/79_pop_front.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+};
+class Linked{
+    public:
+    Node* head;
+    Linked(){
+    head=NULL;
+    }
+    void Pushfront(int val){
+        Node* newNode = new Node(val);
+       newNode->next=head;
+       head=newNode;
+    }
+    void popfront(){
+        if(head==NULL){
+            cout << " Linked list iis empty " << endl;
+        }
+        Node* temp=head;
+        head = head->next;
+        delete temp;
+    }
+    void display(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout << temp->data << " ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+  Linked ll;
+  ll.Pushfront(11);
+  ll.Pushfront(12);
+  cout << " Without Pop-Front " << endl;
+  ll.display();
+  cout << " With Pop-Front " << endl;
+  ll.popfront();
+  ll.display();
+  return 0;
+};
